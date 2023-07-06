75432690c573f77f24938c5e837435e1d3bd334e
¨kemken077¨ <kemken077@gmail.com>
2023-07-06 19:48:28 -0300

    Add nodemon and front end files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3002;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello Sal!');
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
