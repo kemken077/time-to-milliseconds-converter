4c2cd5d38f77b4d9ceecad77d53575bbf0907a60
¨kemken077¨ <kemken077@gmail.com>
2023-07-06 22:33:13 -0300

    JavaScript calculation for minutes and seconds

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,22 @@
-console.log('App JS! [Frontend]');
+/**
+ *
+ *
+ * 1 minute = 60000 milliseconds
+ * 1 second = 1000 milliseconds
+ *
+ */
+const minutesInput = document.getElementById('minutes');
+const secondsInput = document.getElementById('seconds');
+const millisecondsDisplayElement = document.getElementById('milliseconds');
+const oneMinuteInMilliseconds = 60000;
+const oneSecondInMilliseconds = 1000;
+
+const getMinutesAsMilliseconds = (min) => min * oneMinuteInMilliseconds;
+const getSecondsAsMilliseconds = (seconds) => seconds * oneSecondInMilliseconds;
+
+function timeToMilliseconds(minutes, seconds) {
+  const minutesInMilliseconds = getMinutesAsMilliseconds(minutes);
+  const secondsInMilliseconds = getSecondsAsMilliseconds(seconds);
+
+  return minutesInMilliseconds + secondsInMilliseconds;
+}
